d6e3ecc74513171cb160a80f5fd4b477b00a5507
Rasmus Svala <rasmus.svala@hotmail.com>
2024-10-31 17:43:31 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,9 @@
 # Create the directory for Neovim if it doesn't exist
 mkdir -p ~/.config/nvim
 
-# Create symlinks for Zsh configuration
+# Create symlinks for zsh, neovim and git
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-
-# Optional: Create a symlink for Windows Terminal settings (if applicable)
-# Note: You can manually copy the settings file later
-# mkdir -p ~/dotfiles/windows_terminal
-# cp "C:\Users\<YourUsername>\AppData\Local\Packages\Microsoft.WindowsTerminal_<some_hash>\LocalState\settings.json" ~/dotfiles/windows_terminal/settings.json
 
 echo "Dotfiles have been symlinked!"

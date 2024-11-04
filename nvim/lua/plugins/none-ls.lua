cb4925509fdd207cf554fccf0c8328ad7bc5b34f
Rasmus Svala <rasmus.svala@hotmail.com>
2024-11-04 19:04:13 +0100

    added ensure_install in none ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,37 @@
--- formatter and linters
+-- Initialize Mason and Null-LS
+require("mason").setup()
+
+-- Function to ensure tools are installed
+local function ensure_installed(servers)
+	local mason_registry = require("mason-registry")
+	for _, server in ipairs(servers) do
+		local pkg = mason_registry.get_package(server)
+		if not pkg:is_installed() then
+			pkg:install()
+		end
+	end
+end
 
 return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+
+		-- List of tools to ensure are installed
+		local tools = {
+			"stylua",
+			"prettier",
+		}
+
+		-- Ensure the tools are installed
+		ensure_installed(tools)
+
+		-- Set up null-ls with the installed tools
 		null_ls.setup({
 			sources = {
-				-- make sure they are all installed in mason
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				-- null_ls.builtins.diagnostics.eslint_d, -- can't get it to work atm, will check later
+				-- null_ls.builtins.diagnostics.eslint_d,
 				-- null_ls.builtins.diagnostics.htmlhint,
 			},
 		})

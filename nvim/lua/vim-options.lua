3ee3ae0be939c7898c5ebf33b13295526b752570
Rasmus Svala <rasmus.svala@hotmail.com>
2024-11-04 17:36:22 +0100

    integrated windows clipboard

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -7,3 +7,23 @@ vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Clipboard integration for Neovim on WSL
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = "powershell.exe -c Get-Clipboard",
+      ["*"] = "powershell.exe -c Get-Clipboard",
+    },
+    cache_enabled = 0,
+  }
+end
+
+vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.keymap.set('v', '<C-v>', '"+p', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-v>', '"+p', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })

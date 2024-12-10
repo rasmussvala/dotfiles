71e8aa0ae7b8a95180bdcfed76a5d7dff8b0e01c
Rasmus Svala <rasmus.svala@hotmail.com>
2024-12-10 21:49:41 +0100

    added colorls to dotfiles

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 # Remove existing config directories if they exists
 rm -rf ~/.config/nvim
 rm -rf ~/.config/kitty
+rm -rf ~/.config/colorls
 
 # Create directories
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/kitty
+mkdir -p ~/.config/colorls
 
 # Create symlink
 ln -sf ~/dotfiles/kitty/* ~/.config/kitty/
 ln -sf ~/dotfiles/nvim/* ~/.config/nvim/
+ln -sf ~/dotfiles/colorls/* ~/.config/colorls/
 
 echo "Dotfiles have been symlinked!"
 
